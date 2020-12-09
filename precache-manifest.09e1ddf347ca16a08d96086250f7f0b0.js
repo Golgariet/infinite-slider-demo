72076482ba176861ffc46831e1f8e5c247303d58
Anton Bibergal <nathgilt@gmail.com>
2020-12-09 15:22:04 +0200

    Updates

---
+++ precache-manifest.09e1ddf347ca16a08d96086250f7f0b0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67653fc4796b3d30ef36b32998fe856e",
+    "url": "/infinite-slider-demo/index.html"
+  },
+  {
+    "revision": "7138677abc74577cb408",
+    "url": "/infinite-slider-demo/static/css/main.99c517f9.chunk.css"
+  },
+  {
+    "revision": "b3f0b1fe0dacb184d5f6",
+    "url": "/infinite-slider-demo/static/js/2.4e5ed404.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infinite-slider-demo/static/js/2.4e5ed404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7138677abc74577cb408",
+    "url": "/infinite-slider-demo/static/js/main.9baadaac.chunk.js"
+  },
+  {
+    "revision": "4daac73907d596ea9197",
+    "url": "/infinite-slider-demo/static/js/runtime-main.25ed2510.js"
+  },
+  {
+    "revision": "2c69353f4978f9f610ecb128c907084b",
+    "url": "/infinite-slider-demo/static/media/game-1.2c69353f.jpg"
+  },
+  {
+    "revision": "01cce3d779c979798fefbf20f26fa0f1",
+    "url": "/infinite-slider-demo/static/media/game-2.01cce3d7.jpg"
+  },
+  {
+    "revision": "1a689743f0b4fbbd798c2712df447ccb",
+    "url": "/infinite-slider-demo/static/media/game-3.1a689743.jpg"
+  },
+  {
+    "revision": "976b6c91cb169dec454ecabb6867deb0",
+    "url": "/infinite-slider-demo/static/media/game-4.976b6c91.jpg"
+  },
+  {
+    "revision": "2ae3dd45cb3a5c66996bfd6f0e6fce98",
+    "url": "/infinite-slider-demo/static/media/game-5.2ae3dd45.jpg"
+  }
+]);
